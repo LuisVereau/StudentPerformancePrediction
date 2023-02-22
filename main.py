ee12fa65484afb89f153199ce018681af35de1c4
Luis Vereau <luis.vereau@pentago.com.br>
2023-02-22 11:35:00 -0300

    removing a print()

--- main.py
+++ main.py
@@ -111,7 +111,6 @@ my_models = list()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed[1])
 
-print(len(X_train))
 for train, val in kf.split(X_train, y_train):
     fold += 1
 
